eda90a2ba05fb34e2b2f9f9b272d674a98a9260d
leatherbullet <al.obnizov@yandex.ru>
2024-02-28 09:08:18 +0300

    Добавьте файлы проекта.

---
+++ имя/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace имя
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            string userSimbol;
+            string simbols = "";
+
+            Console.WriteLine("введите имя:");
+            userName = Console.ReadLine();
+            Console.WriteLine("введите символ:");
+            userSimbol = Console.ReadLine();
+
+            for (int i = 0 ; i <= userName.Length + 1 ; i++)
+            {
+                simbols += userSimbol;
+            }
+
+            Console.Clear();
+            Console.WriteLine(simbols);
+            Console.WriteLine(userSimbol + userName + userSimbol);
+            Console.WriteLine(simbols);
+        }
+    }
+}
---
+++ имя/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("имя")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("имя")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c1bef476-29ac-4d1b-a046-7b96934cdc50")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
